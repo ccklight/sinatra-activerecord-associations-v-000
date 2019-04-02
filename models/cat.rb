044e89b36c4661c3621e06fc3efaeb547a9f033e
Cheryl K <cherylkelson@icloud.com>
2019-04-02 12:08:49 +0000

    Automatically backed up by Learn

--- models/cat.rb
+++ models/cat.rb
@@ -1,3 +1,14 @@
 class Cat < ActiveRecord::Base
+  def change
+    create_table :cats do |t|
+
+    t.string :name
+    t.string :age
+    t.string :breed
+        end
+    end
+
+
+
   belongs_to :owner
 end
